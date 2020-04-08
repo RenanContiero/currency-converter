d02d7e8ace20053ec0152b57bdbe00cf12d573c0
lookfire <renancontiero@hotmail.com>
2020-04-07 22:44:04 -0300

    gerado e implementado o euro.component.ts

--- currency/src/app/app.module.ts
+++ currency/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DolarComponent } from './dolar/dolar.component';
+import { EuroComponent } from './euro/euro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DolarComponent
+    DolarComponent,
+    EuroComponent
   ],
   imports: [
     BrowserModule,
