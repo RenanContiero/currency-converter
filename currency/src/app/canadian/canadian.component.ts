9775d292174b187caa6bd2bad72f762f50dcd1fa
lookfire <renancontiero@hotmail.com>
2020-04-13 10:14:49 -0300

    implementado a api no canadian.component.ts

--- currency/src/app/canadian/canadian.component.ts
+++ currency/src/app/canadian/canadian.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import { ApiService } from "../api.service"
 
 @Component({
   selector: 'app-canadian',
@@ -9,25 +10,35 @@ import { DomSanitizer } from "@angular/platform-browser";
 })
 export class CanadianComponent implements OnInit {
 
-  constructor() { }
+  persondata: any;
+  numberResult:number;
+  cad:number;
+  display:boolean;
 
-  ngOnInit(): void {
-  }
+  constructor(private myservice: ApiService) { }
 
-  valueDolar:number = 3.74;
+  ngOnInit() {
+    this.myservice.getData('CAD').subscribe((data) => {
+      this.persondata =  Object.create(data).rates;
+      console.log(this.persondata);
+      this.cad = this.persondata.BRL.toFixed(2);
+
+    })
+  };  
+
   numberResult:number;
 
   calculateDolarCanadian(event){
     let result = event.target.value;
     this.numberResult = Number(result);
-    this.numberResult = this.numberResult * this.valueDolar;
+    this.numberResult = this.numberResult * this.cad;
     console.log(this.numberResult);
   }
 
   calculateReal(event){
     let result = event.target.value;
     this.numberResult = Number(result);
-    this.numberResult = this.numberResult / this.valueDolar;
+    this.numberResult = this.numberResult / this.cad;
     console.log(this.numberResult);
   }
 
